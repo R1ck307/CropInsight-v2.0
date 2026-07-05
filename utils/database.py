c4d22da38f69acec4ffd1f62e9b13285a73198cb
R1ck307 <boylilritchie@gmail.com>
2026-07-06 01:29:21 +0200

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+
 class Database:
 
     def __init__(self, path):
@@ -8,26 +9,22 @@ class Database:
         self.ensure_file()
 
     def ensure_file(self):
-    folder = os.path.dirname(self.path)
-    if folder:
-        os.makedirs(folder, exist_ok=True)
+        folder = os.path.dirname(self.path)
+        if folder:
+            os.makedirs(folder, exist_ok=True)
 
-    if not os.path.exists(self.path):
-        pd.DataFrame().to_csv(self.path, index=False)
-    
+        if not os.path.exists(self.path):
+            pd.DataFrame().to_csv(self.path, index=False)
+
     def load(self):
-    import pandas as pd
+        if not os.path.exists(self.path):
+            return pd.DataFrame()
 
-    if not os.path.exists(self.path):
-        return pd.DataFrame()
-
-    if os.path.getsize(self.path) == 0:
-        return pd.DataFrame()
-
-    try:
-        return pd.read_csv(self.path)
-    except pd.errors.EmptyDataError:
-        return pd.DataFrame()
+        try:
+            df = pd.read_csv(self.path)
+            return df
+        except pd.errors.EmptyDataError:
+            return pd.DataFrame()
 
     def save(self, df):
         df.to_csv(self.path, index=False)
@@ -44,25 +41,24 @@ class Database:
         if df.empty:
             return False
 
-        mask = df[condition_col] == condition_val
-
-        for key, value in updates.items():
-            df.loc[mask, key] = value
-
+        df.loc[df[condition_col] == condition_val, list(updates.keys())] = list(updates.values())
         self.save(df)
         return True
 
     def delete(self, condition_col, condition_val):
         df = self.load()
 
-        df = df[df[condition_col] != condition_val]
+        if df.empty:
+            return False
 
+        df = df[df[condition_col] != condition_val]
         self.save(df)
         return True
 
     def find(self, condition_col, condition_val):
         df = self.load()
 
-        result = df[df[condition_col] == condition_val]
+        if df.empty:
+            return pd.DataFrame()
 
-        return result
+        return df[df[condition_col] == condition_val]
