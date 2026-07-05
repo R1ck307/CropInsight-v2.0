8b3c7cc358518c45015b6b9a9b8608621580db9f
R1ck307 <boylilritchie@gmail.com>
2026-07-06 00:19:14 +0200

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,43 +1,50 @@
-import hashlib
-import json
+import pandas as pd
 import os
+from datetime import datetime
 
-USERS_FILE = "data/users.json"
+USER_DB_PATH = "database/users.csv"
 
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
 
 def load_users():
-    if not os.path.exists(USERS_FILE):
-        return {}
-    with open(USERS_FILE, "r") as f:
-        return json.load(f)
+    if not os.path.exists(USER_DB_PATH):
+        df = pd.DataFrame(columns=["id", "username", "password", "role", "created_at"])
+        df.to_csv(USER_DB_PATH, index=False)
+        return df
+    return pd.read_csv(USER_DB_PATH)
 
-def save_users(users):
-    with open(USERS_FILE, "w") as f:
-        json.dump(users, f, indent=4)
 
-def register_user(username, password, role="farmer"):
-    users = load_users()
+def save_users(df):
+    df.to_csv(USER_DB_PATH, index=False)
 
-    if username in users:
-        return False, "User already exists"
 
-    users[username] = {
-        "password": hash_password(password),
-        "role": role
+def register_user(username, password, role="user"):
+    df = load_users()
+
+    if username in df["username"].values:
+        return False, "Username already exists"
+
+    new_id = len(df) + 1
+
+    new_user = {
+        "id": new_id,
+        "username": username,
+        "password": password,
+        "role": role,
+        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
-    save_users(users)
+    df = pd.concat([df, pd.DataFrame([new_user])], ignore_index=True)
+    save_users(df)
+
     return True, "User registered successfully"
 
+
 def login_user(username, password):
-    users = load_users()
+    df = load_users()
 
-    if username not in users:
-        return False, "User not found"
+    user = df[(df["username"] == username) & (df["password"] == password)]
 
-    if users[username]["password"] != hash_password(password):
-        return False, "Incorrect password"
+    if not user.empty:
+        return True, user.iloc[0].to_dict()
 
-    return True, users[username]["role"]
+    return False, "Invalid username or password"
