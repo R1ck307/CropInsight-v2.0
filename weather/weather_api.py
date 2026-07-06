dd0ebad76635fbf9428d78530f49d1acd7951e54
R1ck307 <boylilritchie@gmail.com>
2026-07-06 23:35:54 +0200

    Update weather_api.py

--- weather/weather_api.py
+++ weather/weather_api.py
@@ -1,1 +1,19 @@
+from datetime import datetime
 
+
+def get_weather(location):
+    """
+    Temporary weather provider.
+    Later we can connect a real API.
+    """
+
+    weather = {
+        "location": location,
+        "temperature": 25,
+        "humidity": 70,
+        "rainfall": 10,
+        "condition": "Partly Cloudy",
+        "date": datetime.now().strftime("%Y-%m-%d")
+    }
+
+    return weather
